94a7f65a92a8fe682c23e9a5d644723eaebd7a5f
Ilias <ilias.dinia@outlook.com>
2024-07-07 21:24:21 +0300

    Refactoring code in the textarea component

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -3,22 +3,17 @@ import Warning from "./Warning";
 
 export default function TextArea() {
   const [text, setText] = useState("");
-  const [showWarning, setShowWarning] = useState(false);
   const [warningText, setWarningText] = useState("");
 
   const handleChange = (e) => {
     let newText = e.target.value;
     if (newText.includes("<script>")) {
       setWarningText("No script tag allowed!");
-      setShowWarning(true);
+
       newText = newText.replace("<script>", "");
     } else if (newText.includes("@")) {
       setWarningText("No @ symbol allowed");
-      setShowWarning(true);
       newText = newText.replace("@", "");
-    } else {
-      setShowWarning(false);
-      setWarningText("");
     }
     setText(newText);
   };
@@ -31,7 +26,7 @@ export default function TextArea() {
         placeholder="Enter your text"
         spellCheck="false"
       />
-      {showWarning ? <Warning warningText={warningText} /> : null}
+      {warningText ? <Warning warningText={warningText} /> : null}
       <Warning />
     </div>
   );
